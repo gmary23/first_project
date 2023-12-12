4565d819b721f8e68b84ddec8482dc19324359e7
Geila Martins <geila.estudos@gmail.com>
2023-12-12 09:37:50 -0300

    Criando formulários e criando a opção de inserir novos tópicos

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from . models import Topic
+from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request,):
@@ -11,7 +14,21 @@ def topics(request):
     return render(request, 'myapp/topics.html', context)
 
 def topic(request, topic_id):
-    topic = Topic.objects.get(id = topic_id)
-    entries = topic.entry_set.order_by('-date_added')
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('id')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'myapp/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm() # Nenhum dado submetido cria um formulário em branco
+    else:
+        form = TopicForm(request.POST) # Dados POST submetidos - processa os dados
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse(topics)) # essa função reverse -> direciona para o name
+
+    return render(request, 'myapp/new_topic.html', context={'form': form})
+
+
