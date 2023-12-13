26b45874d5b880972f69ea227cd603b47a8bd4e0
Geila Martins <geila.estudos@gmail.com>
2023-12-13 14:52:08 -0300

    Criando logout do usuário

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,8 @@
-
 from django.urls import path
+from django.contrib.auth import views as auth_views # views de autenticação
 from . import views
 
-
-
 urlpatterns = [
-
-    path('login', views.login, name='login')
+    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', views.logout_view, name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,33 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .forms import LoginForm
-from django.contrib.auth import authenticate # vai no banco de dados e verifica os usuários
-from django.contrib.auth import login as authLogin # importa o login direto do authoLogin
+from django.contrib.auth import logout
 
 
-
-def login(request):
-
-    error = False
-
-    if request.method != "POST":
-        form = LoginForm()
-    else:
-        form = LoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST.get('login')
-            password = request.POST.get('senha')
-            user = authenticate(username=username, password=password)
-
-            if user:
-                authLogin(request, user)
-                return HttpResponseRedirect(reverse('index'))
-            else:
-                error = True
-
-    context = {'form':form, 'error':error}
-    return render(request, 'users/login.html', context)
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 
 
