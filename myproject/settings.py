c9e876b63e564e5059c9e90ea9893630ff1bcccf
Geila Martins <geila.estudos@gmail.com>
2023-12-13 10:12:41 -0300

    Criando espaço para realizar autenticação no sistema - também configurações no arquivo base.html

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    'myapp', # primeiro app criado
+    'users', # app para login
 ]
 
 MIDDLEWARE = [
