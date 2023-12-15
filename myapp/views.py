cfd3aef78e1774fb6ac7ab07ce6622500cfc9398
Geila Martins <geila.estudos@gmail.com>
2023-12-15 09:44:54 -0300

    Protegendo as informações de usuários, somente ele pode acessar

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required # esse módulo dar permissão ao usuário apenas o que foi ele é dono
 
@@ -11,14 +11,19 @@ def index(request,):
 
 @login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added') # tá pegando todos os objetos que tem como owner o usuário e só depois faz uma ordenação pela data
     context = {'topics': topics}
     return render(request, 'myapp/topics.html', context)
 
 @login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('id')
+    
+    # Garante que o assunto pertence ao usuário atual
+    if topic.owner!= request.user:
+        raise Http404
+
+    entries = topic.entry_set.order_by('-id')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'myapp/topic.html', context)
 
@@ -29,8 +34,10 @@ def new_topic(request):
     else:
         form = TopicForm(request.POST) # Dados POST submetidos - processa os dados
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse(topics)) # essa função reverse -> direciona para o name
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return HttpResponseRedirect(reverse('topics')) # essa função reverse -> direciona para o name
 
     return render(request, 'myapp/new_topic.html', context={'form': form})
 
@@ -38,6 +45,10 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Insere uma nova entrada para o assunto do topico"""
     topic = Topic.objects.get(id=topic_id)
+
+    # Garante que o assunto pertence ao usuário atual
+    if topic.owner!= request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm()
@@ -58,6 +69,9 @@ def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id) # tá buscando na class Entry em models.py o id 
     topic = entry.topic # topic do model topic que está ligado por uma chave estrangeira
 
+    if topic.owner!= request.user:
+        raise Http404
+
     if request.method != 'POST': 
         form = EntryForm(instance=entry) # busca o formulário já preenchido - a instance faz isso - a variável entry traz isso na linha 53
 
@@ -66,4 +80,6 @@ def edit_entry(request, entry_id):
         if form.is_valid():
             form.save
             return HttpResponseRedirect(reverse('topic', args=[topic.id]))
-    return render(request, 'myapp/edit_entry.html', context={'entry': entry, 'topic': topic, 'form': form})
+        
+    context={'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'myapp/edit_entry.html', context)
